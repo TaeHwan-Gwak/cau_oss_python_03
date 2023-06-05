ae5be8e30373c79de42c07552ad6d2a9af3c3b3d
TaeHwan-Gwak <taehw0617@cau.ac.kr>
2023-06-05 18:09:21 +0900

    fix : code & add annotation

--- figure_main.py
+++ figure_main.py
@@ -1,5 +1,3 @@
-# %%
-
 import figure
 
 myline = figure.line(10, 20)
@@ -11,14 +9,18 @@ try:
 except ValueError:
     print("please input positive number for width and height")
 
+myline.set_length(20, 30)
+width, height = myline.get_length()
 try:
-    ellipse = figure.area_ellipse(width, height)
-    print(ellipse)
+    triangle = figure.area_right_triangle(width, height)
+    print(triangle)
 except ValueError:
     print("please input positive number for width and height")
 
+myline.set_length(30, 40)
+width, height = myline.get_length()
 try:
-    right_triangle = figure.area_right_triangle(width, height)
-    print(right_triangle)
+    eclipse = figure.area_eclipse(width, height)
+    print(eclipse)
 except ValueError:
     print("please input positive number for width and height")
